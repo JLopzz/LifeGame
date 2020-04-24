e123e322ed216ede0ca945bbdd823a3d897b87b7
JLopzz <javylopzz@gmail.com>
2020-04-23 20:41:44 -0600

    v0.1 init

---
+++ index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Juego de la Vida</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet" href="./style.css">
+</head>
+<body class ='bg-dark'>
+  <div class="col-md-12 col-lg-10 offset-lg-1 row mt-2">
+    <div class="col-sm-12 col-md-8 d-flex flex-column">
+      <div class="d-block d-md-none">
+        <h1 class="text-light text-center">Juego de la Vida</h1>
+      </div>
+      <div class="d-flex justify-content-center">
+      <table>
+        <?php
+          $max = 125;
+          for ($y=0; $y < $max; $y++) { 
+            echo "<tr>\n";
+            for ($x=0; $x < $max; $x++) { 
+              if(
+                (($x >= 58 && $x <= 60) && $y == 51) ||
+                (($x == 58 || $x == 61) && $y == 52) ||
+                ($x == 58               && ($y >= 53 && $y <= 56)) ||
+                ($x == 61               && $y == 56)
+              ) echo "<td neigh='1' name='tds' class='false' id='$x-$y'></td>\n";
+              else if(
+                ($x == 60               && ($y == 52 || $y == 56)) ||
+                ($x == 61               && ($y == 53 || $y == 55 )) ||
+                ($x == 59               && $y == 56)
+              ) echo "<td neigh='2' name='tds' class='false' id='$x-$y'></td>\n";
+              else if(
+                ($x == 59               && $y == 53) ||
+                ($x == 61               && $y == 54)
+              ) echo "<td neigh='3' name='tds' class='false' id='$x-$y'></td>\n";
+              else if(
+                $x == 59                && $y == 54
+              ) echo "<td neigh='4' name='tds' class='false' id='$x-$y'></td>\n";
+              else if(
+                $x == 59                && $y == 52
+              ) echo "<td neigh='1' name='tds' class='true' id='$x-$y'></td>\n";
+              else if(
+                ($x == 60               && ($y == 53 || $y == 55)) ||
+                ($x == 59               && $y == 55)
+              ) echo "<td neigh='2' name='tds' class='true' id='$x-$y'></td>\n";
+              else if(
+                $x == 60                && $y == 54
+              ) echo "<td neigh='3' name='tds' class='true' id='$x-$y'></td>\n";
+              else echo "<td neigh='0' name='tds' class='false' id='$x-$y'></td>\n";
+            }
+            echo "</tr>\n";
+          }
+        ?>
+      </table>
+      </div>
+    </div>
+    <div class="d-none d-md-block col-md-4 row m-0 px-2">
+      <h1 class= 'text-light text-center'>Juego de la Vida</h1>
+      <div class="col-12 row">
+        <div class="col-7">
+          <label for="itera" class='text-light text-left'>Numero de Generaciones:</label>
+          <input type="text" id="itera" name="itera" class='form-control mb-3' placeholder='Generaciones'>
+        </div>
+        <div class="col-5 roy my-auto p-0">
+          <button onclick="each()" class='btn btn-secondary col-12 mb-1'>Iniciar</button>
+          <a href="./" class='btn btn-primary col-12'>Limpiar</a>
+        </div>
+      </div>
+      <div class='col-12'>
+        <label for="itera" class='text-light text-right mb-0' id='gen'>Generacion: 0</label><br>
+        <label for="itera" class='text-light text-right mb-2' id='alive'>Celulas Vivas: 5</label>
+      </div>
+      <div class="col-12 p-0">
+        <h3 class="text-light">Instrucciones:</h3>
+        <p class="text-light text-justify mb-2">Haz clic en el tablero para revivir las celulas, revive cuantas quieras.</p>
+        <p class="text-light text-justify mb-2">Ahora asigna la cantidad de generaciones que deseas ver en el cuadro de texto, y haz click en <kbd>Iniciar</kbd>.</p>
+        <p class="text-light text-justify mb-2">Y observa como se comportan las celulas a lo largo de las generaciones que pasen.</p>
+        <h3 class="text-light">Reglas del Juego:</h3>
+        <p class="text-light text-justify mb-2">1. La Celula que esté muerta, pero tiene 3 vecinas vivas, vuelve a vivir</p>
+        <p class="text-light text-justify mb-2">2. La celula que esté viva, pero que tenga menos que 2 o mas que 3 vecinos vivos, se muere.</p>
+      </div>
+    </div>
+    <div class="col-sm-12 d-block d-md-none my-3">
+      <div class="row">
+        <div class="col-10 offset-1 row">
+          <input type="text" id="iteraCel" name="itera" class='form-control col-7 m-auto' placeholder='Numero de Generaciones'>
+          <div class="col-5 row m-auto">
+            <button onclick="each()" class='btn btn-secondary col-12 mb-1'>Iniciar</button>
+            <a href="./" class='btn btn-primary col-12'>Limpiar</a>
+          </div>
+          <div class="col-12 row mx-auto my-3">
+            <label for="itera" class='text-light text-center col-6' id='genCel'>Generacion: 0</label>
+            <label for="itera" class='text-light text-center col-6' id='aliveCel'>Celulas Vivas: 5</label>
+          </div>
+        </div>
+        <div class="col-10 offset-1">
+          <h3 class="text-light mt-3">Instrucciones:</h3>
+          <p class="text-light text-justify">Haz clic en el tablero para revivir las celulas, revive cuantas quieras.</p>
+          <p class="text-light text-justify">Ahora asigna la cantidad de generaciones que deseas ver en el cuadro de texto, y haz click en <kbd>Iniciar</kbd>.</p>
+          <p class="text-light text-justify">Y observa como se comportan las celulas a lo largo de las generaciones que pasen.</p>
+          <h3 class="text-light">Reglas del Juego:</h3>
+          <p class="text-light text-justify">1. La Celula que esté muerta, pero tiene 3 vecinas vivas, vuelve a vivir</p>
+          <p class="text-light text-justify">2. La celula que esté viva, pero que tenga menos que 2 o mas que 3 vecinos vivos, se muere.</p>
+        </div>        
+      </div>
+    </div>
+  </div>
+  <script src="./liveGame.js"></script>
+</body>
+</html>
---
+++ liveGame.js
@@ -0,0 +1,157 @@
+var el = document.getElementsByName('tds')
+el.forEach(ele => {
+  ele.onclick = e => {
+    if(e.target.className == 'true') e.target.className='false'
+    else e.target.className='true'
+    getNeigh(ele.id, ele.className)
+    getAlive()
+  }
+});
+
+//obteniendo elementos se ocupa en getNeigh()
+var element = (id,state) =>{
+  let elem = document.getElementById(id)
+  if(elem != null){
+    state=='true'? elem.attributes.neigh.value++ : elem.attributes.neigh.value--
+  }
+}
+
+//obteniendo vecinos para bucle se ocupa en getNeigh()
+var nElement = (id) =>{
+  let elem = document.getElementById(id)
+  if(elem != null){
+    return elem.className == 'true'? true : false
+  }
+}
+
+// obtencion de vecinos
+function getNeigh(id , state, bucle = false){
+  let pX = Number(id.match(/^\d+/g))
+  let pY = Number(id.match(/\d+$/g))
+  let Mx = pX + 1
+  let My = pY + 1
+  let mx = pX - 1
+  let my = pY - 1
+  let res = 0
+  for(let i = 0; i < 8; i++){
+    switch (i) {
+      case 0://(x-1 , y-1)
+        !bucle?element(mx+'-'+my,state):nElement(mx+'-'+my)? res++:0
+        break;
+      case 1://(x-1 , y)
+        !bucle?element(mx+'-'+pY,state):nElement(mx+'-'+pY)? res++:0
+        break;
+      case 2://(x-1 , y+1)
+        !bucle?element(mx+'-'+My,state):nElement(mx+'-'+My)? res++:0
+        break;
+      case 3://(x , y-1)
+        !bucle?element(pX+'-'+my,state):nElement(pX+'-'+my)? res++:0
+        break;
+      case 4://(x , y+1)
+        !bucle?element(pX+'-'+My,state):nElement(pX+'-'+My)? res++:0
+        break;
+      case 5://(x+1 , y-1)
+        !bucle?element(Mx+'-'+my,state):nElement(Mx+'-'+my)? res++:0
+        break;
+      case 6://(x+1 , y)
+        !bucle?element(Mx+'-'+pY,state):nElement(Mx+'-'+pY)? res++:0
+        break;
+      case 7://(x+1 , y+1)
+        !bucle?element(Mx+'-'+My,state):nElement(Mx+'-'+My)? res++:0
+        break;
+      default: break;
+    }
+  }
+  return res
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+var colorElement = (id,state) =>{
+  let elem = document.getElementById(id)
+  elem.className=state
+}
+
+var neighElement = (id,state) =>{
+  let elem = document.getElementById(id)
+  elem.attributes.neigh.value = getNeigh(id,state,true)
+}
+
+async function each(){
+  var iter = document.getElementById('itera').value
+  //bucle
+  for (let a = 0; a < iter; a++) {
+    let state = []
+    el.forEach(ele => {
+      state.push({
+        id: ele.id,
+        state: ele.className,
+        neigh: ele.attributes.neigh.value
+      })
+    })
+    //reglas del juego
+    for(let i in state){
+      if(state[i].state == 'false' && Number(state[i].neigh) == 3 )
+        state[i].state = 'true';
+      else if(state[i].state == 'true' && (Number(state[i].neigh) < 2 || Number(state[i].neigh) > 3))
+        state[i].state = 'false';
+    }
+    for(let i in state) colorElement(state[i].id,state[i].state)
+    for(let i in state) neighElement(state[i].id,state[i].state)
+    getAlive()
+    getGen()
+    iter < 10? await sleep(200):
+      iter < 100? await sleep(100):
+        iter < 200? await sleep(50): await sleep(20)
+    }
+}
+
+function getAlive(){
+  let count = 0
+  //variables de prueba para estado inicial
+  console.clear()
+  let n = {
+    n1:{
+      true:[],
+      false:[]
+    },
+    n2:{
+      true:[],
+      false:[]
+    },
+    n3:{
+      true:[],
+      false:[]
+    },
+    n4:{
+      true:[],
+      false:[]
+    },
+  }
+  //fin de script agregado para pruebas
+  el.forEach(a=>{
+    if(a.className=='true') count++;
+    //if de prueba para estado inicial
+    if(a.attributes.neigh.value==1) n.n1[a.className].push(a)
+    else if(a.attributes.neigh.value==2) n.n2[a.className].push(a)
+    else if(a.attributes.neigh.value==3) n.n3[a.className].push(a)
+    else if(a.attributes.neigh.value==4) n.n4[a.className].push(a)
+    //fin de script agregado para pruebas
+  })
+  console.log(n)
+  document.getElementById('alive').innerText='Celulas Vivas: '+count
+  document.getElementById('aliveCel').innerText='Celulas Vivas: '+count
+}
+
+function getGen(){
+  let num = Number(document.getElementById('gen').innerText.match(/\d+$/g))
+  num++
+  document.getElementById('gen').innerText='Generacion: '+num
+  document.getElementById('genCel').innerText='Generacion: '+num
+}
+
+document.getElementById('iteraCel').oninput = e =>{
+  document.getElementById('itera').value = e.target.value
+}
